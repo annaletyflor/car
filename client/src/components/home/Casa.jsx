9c3958dfb55821757708df9ecf276f5814f71d08
annaletyflor <annaletyflor@gmail.com>
2024-10-25 09:27:48 -0300

    Criação da aba carros disponiveis - Anna e Pietro

--- client/src/components/home/Casa.jsx
+++ client/src/components/home/Casa.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Casa.css"
 import fotopreto from "../../assets/fotofundopreto.svg"
+import carro1 from "../../assets/Carro1.svg"
+import carro2 from "../../assets/Carro2.svg"
+import carro3 from "../../assets/Carro3.svg"
+
 
 const Casa = () => {
   return (
-    <div className="col-12">
+    <div className="col-12 all">
     <div className="foto col-12" style={{ 
   backgroundRepeat:"no-repeat"}}>
 
@@ -23,12 +27,43 @@ const Casa = () => {
   data e hora da devolução
 </h2>
 </div>
-<div className="search-bar col-12">
+<div className="search-bar col-10">
+  <div className="firstcamp col-6">
   <input type="search" name="primeira" id="primeira" />
+  </div>
+  <div className="secondcamp col-4">
   <input type="search" name="segunda" id="segunda" />
+  </div>
+  <div className="tercampc col-4">
   <input type="search" name="terceira" id="terceira" />
+  </div>
 </div>
+<button className="greenbtn col-2 ">
+ <h2>Pesquisar!</h2>
+</button>
  </div>
+    </div>
+    <div className="restHome col-12">
+      <h1>
+      Alguns modelos de carro disponivel:
+      </h1>
+<div className="vrumvrum d-flex">
+<h2>Const nomecar</h2>
+<h2>Const nomecar</h2>
+<h2>Const nomecar</h2>
+      <div className="containercar1  ">
+      
+      <img src={carro1} alt="Hilux"></img>
+      </div>
+      <div className="containercar2 ">
+      
+      <img src={carro2} alt="Santafé"></img>
+      </div>
+      <div className="containercar3 ">
+      
+      <img src={carro3} alt="Toro"></img>
+      </div>
+    </div>
     </div>
     </div>
   );
